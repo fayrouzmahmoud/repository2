e8b03c6dbde0357d0fbab21a9a9e651f2363a679
mohmoud <fayrouz1276@gmail.com>
2019-12-13 19:08:14 +0200

    first

---
+++ app/src/main/java/com/example/myapplication2/Student.java
@@ -0,0 +1,48 @@
+package com.example.myapplication2;
+
+public class Student {
+    String id ;
+    String name ;
+    String level;
+    float avg;
+
+
+    public Student(String id, String name, String level, float avg) {
+        this.id = id;
+        this.name = name;
+        this.level= level;
+        this.avg = avg;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public float getAvg() {
+        return avg;
+    }
+
+    public void setAvg(float avg) {
+        this.avg= avg;
+    }
+}
